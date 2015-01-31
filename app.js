b770359d61a027e4b5227d6dfd9f38b3deba1bfa
Hugo8705 <contact@hugochaume.com>
2015-01-31 01:14:09 +0100

    First commit, chat works, currently working on a user list display

---
+++ app.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = app.listen(7076);
+
+var io = require('socket.io').listen(server);
+
+var connectCounter = 0;
+var clientId = 0;
+
+io.sockets.on('connection', function (socket) {
+
+        // Quand un client se connecte, on met à jour son statut 
+        socket.emit('message', 'Connecté');
+        // on incrémente le compteur de visite
+        connectCounter++;
+        clientId++;
+        // on genere le pseudo par défaut
+        socket.pseudo = "user" + clientId;
+        // on envoit le tout 
+        io.sockets.emit('newConnexion', {compteur:connectCounter, pseudo:socket.pseudo});
+
+        // un utilisateur change son pseudo
+        socket.on('pseudoChanged', function(data){
+            socket.pseudo = data.newPseudo;
+            socket.oldpseudo = data.oldPseudo;
+            io.sockets.emit('aPseudoChanged', {newPseudo : socket.pseudo, oldPseudo : socket.oldpseudo});
+        });
+
+        // un utilisateur parle
+        socket.on('messageSent', function(data){
+            socket.message = data;
+            io.sockets.emit('messageBroadcast', {user : socket.pseudo, message : socket.message});
+        });
+
+        //Quand un client se deconnecte
+        socket.on('disconnect', function(){
+            // Decrementation compteur des connexions
+            connectCounter--;
+            io.sockets.emit('endConnexion', {compteur:connectCounter, pseudo:socket.pseudo});
+        });
+
+});
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ public/javascripts/global.js
@@ -0,0 +1,111 @@
+var socket = io.connect('http://localhost:7076');
+
+$(document).ready(function() {
+
+		// Mise à jour status du client
+		changeStatus();
+
+		// Nouvelle connexion
+		newConnexion();
+
+		// Fermeture connexion
+		endConnexion();
+
+		// Changement de pseudo
+		aPseudoChanged();
+
+		// Envoi Message
+		$('#envoyerMessage').on('click', message);
+
+		// Diffusion Message
+		messageDisplay();
+
+		// Changement pseudo
+		$('#changerPseudo').on('click', setPseudo);
+
+});
+
+function changeStatus() {
+		
+		socket.on('message', function(message) {
+				$('#status').html("Votre status : " + message);
+		})
+
+}
+
+function setPseudo(event) {
+
+		event.preventDefault();
+
+		if($('#inputClientPseudo').val() != '') {
+				var newPseudo = $('#inputClientPseudo').val();
+				var oldPseudo = $('#spanPseudoClient').text();
+				$('#spanPseudoClient').text(newPseudo);
+				$('#inputClientPseudo').val('');
+
+				//on emet l'evenement pour mettre à jour le pseudo
+				socket.emit('pseudoChanged', {newPseudo : newPseudo, oldPseudo : oldPseudo});
+		}	
+
+}
+
+function aPseudoChanged() {
+
+		socket.on('aPseudoChanged', function(data) {
+				var contenu = "<p>" + data.oldPseudo + " s'appelle désormais " + data.newPseudo + "</p>";
+				$('#messages').append(contenu);
+		})
+
+}
+
+function newConnexion() {
+		
+		socket.on('newConnexion', function(message) {
+				$('#nbrConnexion').html("Nombre de personnes connectées : " + message.compteur);
+				$('#spanPseudoClient').text(message.pseudo);
+				var contenu = "<p>" + message.pseudo + " s'est connecté.</p>";
+				$('#messages').append(contenu);
+				$('#listeUsers').append(message.listeUsers);
+		})
+
+}
+
+function endConnexion() {
+		
+		socket.on('endConnexion', function(message) {
+				$('#nbrConnexion').html("Nombre de personnes connectées : " + message.compteur);
+				var contenu = "<p>" + message.pseudo + " s'est déconnecté.</p>";
+				$('#messages').append(contenu);
+				$('#listeUsers').append(message.listeUsers);
+		})
+
+}
+
+function message(event) {
+		
+		event.preventDefault();
+
+		if($('#clientMessage').val() != '') {
+				var messageClient = $('#clientMessage').val();
+				$('#clientMessage').val('')
+				//on emet l'evenement pour mettre à jour le pseudo
+				socket.emit('messageSent', messageClient);
+		}	
+
+}
+
+
+function messageDisplay() {
+
+		socket.on('messageBroadcast', function(data) {
+				var contenu = "<p>" + data.user + " dit : " + data.message + "</p>";
+				$('#messages').append(contenu);
+		})
+
+}
+
+function updateUserList() {
+
+
+
+};
